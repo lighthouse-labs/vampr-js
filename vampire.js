535ce738bd50da394789402b70146890180002c5
hanuz06 <lee1975uz@gmail.com>
2019-11-02 15:39:22 +0000

    added 3 functions

--- vampire.js
+++ vampire.js
@@ -11,7 +11,7 @@ class Vampire {
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
     this.offspring.push(vampire);
-    vampire.creator = this
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
@@ -33,24 +33,56 @@ class Vampire {
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Tree traversal methods **/
 
   // Returns the vampire object with that name, or null if no vampire exists with that name
   vampireWithName(name) {
-    
+    let resName; // 1
+
+    if (this.name === name) {
+      return this;
+    }
+
+    if (this.offspring) {
+      for (const m of this.offspring) {
+        resName = m.vampireWithName(name);
+        if (resName) {
+          return resName;
+        }
+      }
+    }
+    return null;
   }
 
   // Returns the total number of vampires that exist
   get totalDescendents() {
+
+    let total = 0;
     
+    if (this.offspring.length) {
+      for (const offspr of this.offspring) {
+        total += offspr.totalDescendents + 1;
+      }
+    }
+    return total;
   }
 
   // Returns an array of all the vampires that were converted after 1980
   get allMillennialVampires() {
-    
+    let millenials = []; // 1
+
+    if (this.yearConverted > 1980) {
+      millenials.push(this); // 2
+    }
+
+    for (let i of this.offspring) {
+      millenials = millenials.concat(i.allMillennialVampires);
+    }
+
+    return millenials;
   }
 
   /** Stretch **/
@@ -60,8 +92,23 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-  }
+  closestCommonAncestor(vampire) {}
 }
 
-  module.exports = Vampire;
+let magic = new Vampire('beet', 1982);
+let riana = new Vampire('riana', 1982);
+let electro = new Vampire('electro', 1982);
+// let peter = new Vampire('peter', 1982)
+// let hua = new Vampire('hua', 1982)
+
+magic.addOffspring(riana);
+magic.addOffspring(electro);
+// magic.addOffspring(peter)
+// magic.addOffspring(hua)
+
+//console.log(magic)
+console.log('magic name: ',magic.name);
+
+magic.vampireWithName("electro");
+
+module.exports = Vampire;

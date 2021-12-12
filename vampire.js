1b42429dd38197f9a6d32d74197f0bdf6de60073
Caseyann <goore_44@hotmail.ca>
2021-12-12 00:57:04 -0500

    ff

--- vampire.js
+++ vampire.js
@@ -6,26 +6,45 @@ class Vampire {
     this.creator = null;
   }
 
+
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
+
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let numberOfVamps = 0;
+    let currentVamp = this;
 
+    // climb "up" the tree (using iteration), counting nodes, until no boss is found
+    while (currentVamp.creator) {
+      currentVamp = currentVamp.creator;
+      numberOfVamps++;
+    }
+
+    return numberOfVamps;
   }
+
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    const rankVamp1 = this.numberOfVampiresFromOriginal;
+    const rankVamp2 = vampire.numberOfVampiresFromOriginal;
+    if (rankVamp2 > rankVamp1) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   /** Stretch **/
@@ -35,10 +54,31 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
+  getAllAncestors() {
+    let currentAncestors = [];
+    for(let coven = this; (coven); coven = coven.creator) {
+      currentAncestors.push(coven);
+    }
+    return currentAncestors;
+  }
+
   closestCommonAncestor(vampire) {
+    let currentAncestors = this.getAllAncestors();
+    let vampyAncestors = vampire.getAllAncestors();
+    for(const currentAncestor of currentAncestors) {
+      if(!currentAncestor) return;
+      for (const vampyAncestor of vampyAncestors) {
+        if (!vampyAncestor) return;
+        if (currentAncestor.name === vampyAncestor.name) {
+          return currentAncestor;
+        }
+      }
+    }
 
   }
 }
+
+
 
 module.exports = Vampire;
 

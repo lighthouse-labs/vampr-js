627c42150cf8b96e358458a98ff3618b09b3da7b
Situ2000 <yuanyuansitu@gmail.com>
2023-07-06 01:22:21 +0000

    Answers

--- vampire.js
+++ vampire.js
@@ -10,22 +10,29 @@ class Vampire {
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
-
+    let count = 0;
+    let currentVampire = this;
+    while (currentVampire.creator !== null) {
+      count++;
+      currentVampire = currentVampire.creator;
+    }
+    return count;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
-
+    return this.numberOfVampiresFromOriginal < vampire.numberOfVampiresFromOriginal;
   }
 
   /** Stretch **/
@@ -36,7 +43,18 @@ class Vampire {
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
   closestCommonAncestor(vampire) {
+    let currentVampireA = this;
+    let currentVampireB = vampire;
 
+    while (currentVampireA !== currentVampireB) {
+      if (currentVampireA.creator === null || currentVampireB.creator === null) {
+        return null; // No common ancestor found
+      }
+      currentVampireA = currentVampireA.creator;
+      currentVampireB = currentVampireB.creator;
+    }
+
+    return currentVampireA;
   }
 }
 
